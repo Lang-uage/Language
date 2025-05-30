354172f123e871a9e522f070cb56a89918436b12
TanviKandepuneni <tkandepuneni@umass.edu>
2025-05-29 20:51:06 -0400

    Reverting to a better version

--- docker/frontend/start-nginx.sh
+++ docker/frontend/start-nginx.sh
@@ -13,7 +13,7 @@ if [ -z "$FRONTEND_PORT" ]; then
   FRONTEND_PORT="$2"
 fi
 if [ -z "$FRONTEND_PORT" ]; then
-  FRONTEND_PORT="3000"
+  FRONTEND_PORT="80"
 fi
 if [ -z "$BACKEND_URL" ]; then
   echo "BACKEND_URL must be set as an environment variable or as first parameter. (e.g. http://localhost:7860)"
@@ -24,7 +24,7 @@ fi
 export BACKEND_URL FRONTEND_PORT
 
 # Use envsubst to substitute environment variables in the template
-envsubst '${BACKEND_URL} ${FRONTEND_PORT}' < /etc/nginx/conf.d/default.conf > $CONFIG_DIR/default.conf
+envsubst '${BACKEND_URL} ${FRONTEND_PORT}' < /etc/nginx/conf.d/default.conf.template > $CONFIG_DIR/default.conf
 
 # Start nginx with the new configuration
-exec nginx -g 'daemon off;'
+exec nginx -c $CONFIG_DIR/default.conf -g 'daemon off;'
